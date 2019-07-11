1e56dfd098972813c44d7d4eeb63fddc76ad1b39
Frederic Poitevin <frederic.poitevin@gmail.com>
2019-07-11 16:55:01 -0700

    corrected download function in osf_interact. Tested in Identify a directory and download its content notebook

--- src/osf_interact.py
+++ src/osf_interact.py
@@ -4,16 +4,41 @@ import requests
 import numpy as np
 import urllib.request
 
-def download_all_files(PROJECT_URL, PROJECT_DIR, fileidlist, filenamelist):
+def download_per_page(PROJECT_URL, PROJECT_DIR, DIR_ID=None, ext=None, start_page=-1, end_page=-1):
+    """
+    download_per_page
+    """
+    if not os.path.exists(PROJECT_DIR):
+        os.makedirs(PROJECT_DIR)
+    # How many pages do we need to go through ?
+    DIR_URL = PROJECT_URL+DIR_ID+'/'
+    first_page = 1
+    last_page  = url_to_npages(DIR_URL)
+    if(end_page > 0):
+        if(end_page < last_page):
+            last_page = end_page
+    if(start_page > 0):
+        first_page = start_page
+    print('About to skim through {0} pages'.format(last_page - first_page + 1))
+    # Let's skim through them
+    for ipage in np.arange(first_page, last_page+1):
+        names, ids = url_to_idlist(DIR_URL+'/?page='+str(ipage), kind='file', ext=ext)
+        print('Number of hits in page {0}: {1}'.format(ipage, len(ids)))
+        download_files_from_list(PROJECT_URL, PROJECT_DIR, ids, names)
+
+def download_files_from_list(PROJECT_URL, PROJECT_DIR, fileidlist, filenamelist):
     """
     """
     if not os.path.exists(PROJECT_DIR):
         os.makedirs(PROJECT_DIR)
     for i in np.arange(len(filenamelist)):
         url  = PROJECT_URL+fileidlist[i]
-        path = PROJECT_DIR+filenamelist[i] 
-        print('... Downloading {0} to {1} ...'.format(url, path))
-        urllib.request.urlretrieve(url, path)
+        path = PROJECT_DIR+filenamelist[i]
+        if not os.path.exists(path):
+            urldownload = get_url(url)['data']['links']['download']
+            print('... Downloading {0} to {1} ...'.format(urldownload, path))
+            urllib.request.urlretrieve(urldownload, path)
+            
 
 def list_all_files(url, ext=None, npages_max=-1):
     """
