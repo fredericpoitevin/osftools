7e7b997a9d5b99bac51ec926ac3ec5258f4e0c63
fredericpoitevin <frederic.poitevin@gmail.com>
2019-07-12 11:41:23 -0700

    debugged verbosity in osf_interact

--- src/osf_interact.py
+++ src/osf_interact.py
@@ -20,15 +20,19 @@ def download_per_page(PROJECT_URL, PROJECT_DIR, DIR_ID=None, ext=None, start_pag
     if(start_page > 0):
         first_page = start_page
     if(verbose != 'mute'):
-    	print('About to skim through {0} pages'.format(last_page - first_page + 1))
+        print('About to skim through {0} pages'.format(last_page - first_page + 1))
+        if(verbose == 'minimal'):
+            sub_verbose = 'mute'
+    else:
+        sub_verbose = verbose
     # Let's skim through them
     for ipage in np.arange(first_page, last_page+1):
         names, ids = url_to_idlist(DIR_URL+'/?page='+str(ipage), kind='file', ext=ext)
-        if(verbose == 'maximal'):
+        if(verbose != 'mute'):
             print('Number of hits in page {0}: {1}'.format(ipage, len(ids)))
-        download_files_from_list(PROJECT_URL, PROJECT_DIR, ids, names)
+        download_files_from_list(PROJECT_URL, PROJECT_DIR, ids, names, verbose=sub_verbose)
 
-def download_files_from_list(PROJECT_URL, PROJECT_DIR, fileidlist, filenamelist):
+def download_files_from_list(PROJECT_URL, PROJECT_DIR, fileidlist, filenamelist, verbose='minimal'):
     """
     """
     if not os.path.exists(PROJECT_DIR):
@@ -38,7 +42,8 @@ def download_files_from_list(PROJECT_URL, PROJECT_DIR, fileidlist, filenamelist)
         path = PROJECT_DIR+filenamelist[i]
         if not os.path.exists(path):
             urldownload = get_url(url)['data']['links']['download']
-            print('... Downloading {0} to {1} ...'.format(urldownload, path))
+            if(verbose != 'mute'):
+                print('... Downloading {0} to {1} ...'.format(urldownload, path))
             urllib.request.urlretrieve(urldownload, path)
             
 
